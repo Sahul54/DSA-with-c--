ebcbc0b628d8e754b983f0269e61b7da6d93aedf
Sahul54 <ksahul9576@gmail.com>
2024-04-09 12:30:01 +0530

    all Pair

---
+++ Revision Array/Class 2/allPairElement.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int arr[] = {1,2};
+    int size = 2;
+
+    for (int i = 0; i < size; i++){
+        for(int j = 0; j < size; j++){
+            // for(int  k = 0; k < size; k++){
+                cout<<arr[i] <<" "<<arr[j]<<" "<<endl;
+            }
+        }
+    // }
+    
+  return 0;
+}
