1a8cd9d3b557cafc974e2e4aa3ea363143269e60
sahul54 <ksahul9576@gmail.com>
2024-09-14 23:57:47 +0530

    wrap tree

--- Week 11 Queue/Home Work/implementStackUsingQueue.cpp
+++ Week 11 Queue/Home Work/implementStackUsingQueue.cpp
@@ -1,7 +1,63 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+class MyQueue {
+public:
+    stack<int> s1, s2;
 
-return 0;
+    // By default constructor
+    MyQueue() {}
+
+    void push(int x) {
+        s1.push(x);
+    }
+
+    int pop() {
+        if (s2.empty()) {
+            while (!s1.empty()) {
+                s2.push(s1.top());
+                s1.pop();
+            }
+        }
+
+        if (s2.empty()) {
+            throw runtime_error("Queue is empty");
+        }
+
+        int popValue = s2.top();
+        s2.pop();
+        return popValue;
+    }
+
+    int peek() {
+        if (s2.empty()) {
+            while (!s1.empty()) {
+                s2.push(s1.top());
+                s1.pop();
+            }
+        }
+
+        if (s2.empty()) {
+            throw runtime_error("Queue is empty");
+        }
+
+        return s2.top();
+    }
+
+    bool empty() {
+        return s1.empty() && s2.empty();
+    }
+};
+
+int main() {
+    MyQueue queue;
+    queue.push(1);
+    queue.push(2);
+    cout << "Peek: " << queue.peek() << endl; // Should print 1
+    cout << "Pop: " << queue.pop() << endl;   // Should print 1
+    cout << "Peek: " << queue.peek() << endl; // Should print 2
+    cout << "Pop: " << queue.pop() << endl;   // Should print 2
+    cout << "Is empty: " << queue.empty() << endl; // Should print 1 (true)
+
+    return 0;
 }
